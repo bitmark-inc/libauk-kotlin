d7f083f32d896172e50e9926d01a2755dea71f4e
phuoc <phuoc@bitmark.com>
2024-09-11 14:09:40 +0700

    unit test
    Signed-off-by: phuoc <phuoc@bitmark.com>

--- libauk/src/test/java/com/bitmark/libauk/LibAukUnitTest.kt
+++ libauk/src/test/java/com/bitmark/libauk/LibAukUnitTest.kt
@@ -22,4 +22,16 @@ class LibAukUnitTest {
         val result = libAuk.calculateFirstEthAddress(words, passphrase);
         Assert.assertEquals(expectedAddress, result);
     }
+
+    @Test
+    fun testGetAccountDID {
+        val words = "daring mix cradle palm crowd sea observe whisper rubber either uncle oak"
+        libAuk.importKey(words);
+
+        val expectedDiD = "did:key:zQ3shUnBWE7Dkskaozsnzsb78kVcgQFbtXf7zdCCDN3qepBGL";
+        val expectedSig = "3045022100bcab09830ca590e641db881d9642ea2372cecedc1a37647e9d6ab8365521b7c0022041cba853b76596a64baf909aa311a18ae4d79c88aec15a080a897e3266e44aa2"
+        val accountDID = libAuk.getAccountDID();
+        val sig = libAuk.getAccountDIDSignature("hello");
+        Assert.assertEquals(expectedAddress, result);
+    }
 }
